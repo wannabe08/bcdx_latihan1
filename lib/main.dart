de8a25c14be5be966d709faadf119c84d368c8c2
Pablo <playerwan12@gmail.com>
2023-10-11 20:32:32 +0700

    commit tanggal 11-10-2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,6 +31,120 @@ class MyApp extends StatelessWidget {
                 Text("data 4 "),
               ],
             ),
+            Container(
+              height: 30,
+              width: 500,
+              color: Colors.pink,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+              height: 30,
+              width: 30,
+              color: Colors.pink
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              color: Colors.pink
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              color: Colors.pink
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              color: Colors.pink
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              color: Colors.pink
+            ),
+            
+            
+
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                shape: BoxShape.circle,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                shape: BoxShape.circle,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                shape: BoxShape.circle,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                shape: BoxShape.circle,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                shape: BoxShape.circle,
+              ),
+            ),
+              ],
+            ),
+            
           ],
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:latihan1/main.dart';
+import 'package:bcdx_latihan1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
